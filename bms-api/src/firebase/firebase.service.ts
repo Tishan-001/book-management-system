00f50eb29b3ebb540c66176a0dd2b0e658afc8e3
Tishan-001 <tishanshamika200@gmail.com>
2024-08-03 00:01:12 +0530

    update 2nd Aug

--- bms-api/src/firebase/firebase.service.ts
+++ bms-api/src/firebase/firebase.service.ts
@@ -6,14 +6,14 @@ export class FirebaseService {
   private readonly storage: admin.storage.Storage;
 
   constructor() {
-    // // eslint-disable-next-line @typescript-eslint/no-var-requires
-    // const serviceAccount = require('../../book-management.json');
-    // admin.initializeApp({
-    //   credential: admin.credential.cert(serviceAccount),
-    //   storageBucket: 'book-management-system-20dc7.appspot.com',
-    // });
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const serviceAccount = require('../../book-management.json');
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      storageBucket: 'book-management-system-20dc7.appspot.com',
+    });
 
-    // this.storage = admin.storage();
+    this.storage = admin.storage();
   }
 
   getStorageInstance(): admin.storage.Storage {
